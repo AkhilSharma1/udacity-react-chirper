002d28b08419f7047388f2cd5083eb225d0702af
Akhil Sharma <akhil.sharma0910@gmail.com>
2018-08-17 22:32:39 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+function handleData () {
+  return (dispatch) => {}
+ }
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <div>
+        Starter Code
+      </div>
+    )
+  }
+}
+
+export default App
